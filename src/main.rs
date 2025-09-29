6a3801aff88ad67027645925ca87554ce05ef5a4
Paul Jaros <jaros.paul@gmail.com>
2025-09-29 07:58:57 +0200

    - Set initial date to closest past (or today'ss) monday - Add "<", ">" buttons to increase/decrease date by a week

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
-use chrono::Local;
+use chrono::{Datelike, Days, Local};
 use eframe::egui;
 use eframe::egui::TextEdit;
 use eframe::egui::Visuals;
@@ -18,11 +18,15 @@ fn main() -> eframe::Result {
     const EDIT_WIDTH: f32 = 200.0;
     const TITLE: &str = "Menu → PDF";
 
-    let mut datum = Local::now().date_naive();
     let mut montag_mittag = "".to_owned();
     let mut montag_abend = "".to_owned();
     let mut dienstag_mittag = "".to_owned();
     let mut dienstag_abend = "".to_owned();
+
+    // Calculate closest past (or today's) monday
+    let mut datum = Local::now().date_naive();
+    let day :u64 = (datum.weekday().number_from_monday() - 1).into();
+    datum = datum.checked_sub_days(Days::new(day)).to_owned().unwrap();
 
     eframe::run_simple_native(TITLE, options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -31,12 +35,19 @@ fn main() -> eframe::Result {
             ctx.set_pixels_per_point(1.5);
 
             ui.horizontal(|ui| {
-                let datum_label = ui.label("Datum: ");
-                ui.add(DatePickerButton::new(&mut datum).format("%e. %b %Y"))
-                    .labelled_by(datum_label.id);
+                ui.label("Datum: ");
+                let left_button = ui.button("<");
+                if left_button.clicked() {
+                    datum = datum.checked_sub_days(Days::new(7)).to_owned().unwrap();
+                };
+                ui.add(DatePickerButton::new(&mut datum).format("%e. %b %Y"));
+                let right_button = ui.button(">");
+                if right_button.clicked() {
+                    datum = datum.checked_add_days(Days::new(7)).to_owned().unwrap();
+                }
             });
-            // TODO: Add two buttons left and right with arrows to decrement and increment by a week
-            // TODO: Search why DatePickerButton is darker than TextEdit::multiline
+
+            // TODO: On date selection: Check if a monday was selected. If not, correct it to the next past monday
             // TODO: Add https://crates.io/crates/pretty_ini as a dependency and use it to save and restore the state
             // TODO: Replace variables with array for each day
             egui::Grid::new("grid_id").show(ui, |ui| {
