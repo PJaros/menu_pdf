d5960a42da03ff1bc1f00ce2a3fa353cc682af90
Paul Jaros <jaros.paul@gmail.com>
2025-10-03 16:00:25 +0200

    - rustfmt: Set (temporarily) line length to 120 - Test array to be used on ui::gui - Test Ini load and save

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,26 @@ use chrono::{Datelike, Days, Local};
 use eframe::egui;
 use eframe::egui::TextEdit;
 use eframe::egui::Visuals;
+// use eframe::egui::{TextEdit, Ui};
 use egui_extras::DatePickerButton;
+
+// use ini::Ini;
+// use std::path::Path;
+
+const EDIT_WIDTH: f32 = 200.0;
+const TITLE: &str = "Menu → PDF";
+const TIME_LONG: [&str; 2] = ["Mittag", "Abend"];
+const _TIME_SHORT: [&str; 2] = ["mi", "ab"];
+const _WEEK_LONG: [&str; 7] = [
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Samstag",
+    "Sonntag",
+];
+const _WEEK_SHORT: [&str; 7] = ["mo", "di", "mi", "do", "fr", "sa", "so"];
 
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -15,9 +34,25 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
 
-    const EDIT_WIDTH: f32 = 200.0;
-    const TITLE: &str = "Menu → PDF";
+    /*
+        const INI_FILE_PATH: &'static str = "menu.ini";
+        let ini_path = Path::new(INI_FILE_PATH);
+        println!("{}", ini_path.exists());
 
+        let mut conf;
+        if (ini_path.exists()) {
+            conf = Ini::load_from_file(ini_path).expect("Error loading ini file");
+        } else {
+            conf = Ini::new();
+        }
+        conf.write_to_file(ini_path)
+            .expect("Error writing ini file");
+    */
+
+    let mut _array_sting = ["1".to_string(), "2".to_string()];
+    let mut _array_sting2 = [["1".to_string(), "2".to_string()], ["3".to_string(), "4".to_string()]];
+    let mut _array_str: [&str; 2] = ["1", "2"];
+    let mut _array2_str: [[&str; 2]; 2] = [["1", "2"], ["3", "4"]];
     let mut montag_mittag = "".to_owned();
     let mut montag_abend = "".to_owned();
     let mut dienstag_mittag = "".to_owned();
@@ -25,7 +60,7 @@ fn main() -> eframe::Result {
 
     // Calculate closest past (or today's) monday
     let mut datum = Local::now().date_naive();
-    let day :u64 = (datum.weekday().number_from_monday() - 1).into();
+    let day: u64 = (datum.weekday().number_from_monday() - 1).into();
     datum = datum.checked_sub_days(Days::new(day)).to_owned().unwrap();
 
     eframe::run_simple_native(TITLE, options, move |ctx, _frame| {
@@ -47,13 +82,13 @@ fn main() -> eframe::Result {
                 }
             });
 
+            // TODO: Use arrays to retain and read/write them ini file
             // TODO: On date selection: Check if a monday was selected. If not, correct it to the next past monday
-            // TODO: Add https://crates.io/crates/pretty_ini as a dependency and use it to save and restore the state
-            // TODO: Replace variables with array for each day
             egui::Grid::new("grid_id").show(ui, |ui| {
                 ui.label("");
-                ui.label("Mittag");
-                ui.label("Abend");
+                for s in TIME_LONG.into_iter() {
+                    ui.label(s);
+                }
                 ui.end_row();
 
                 ui.label("Montag");
